025ee7dde30f14455a21d704c1524c50c23aab63
Josias Kayan Lima <josiaskayanlima@outlook.com>
2019-04-20 01:47:00 -0400

    Deploy Heroku 3

--- config/config.js
+++ config/config.js
@@ -1,11 +1,17 @@
-const env = process.env.NODE_ENV ;
-const envmongo = process.env.NODE_MONGO ;
+const env = process.env.NODE_ENV || 'production' ;
+const envmongo = process.env.NODE_MONGO || 'google.com' ;
 
 const config = () => {
 
     switch(env){
 
         case 'dev':
+        return{
+            bd_string:envmongo,
+            jwt_password: 'lojavirtual',
+            jwt_time:'7d'
+        }
+        case 'production':
         return{
             bd_string:envmongo,
             jwt_password: 'lojavirtual',
